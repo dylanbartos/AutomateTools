3dedfa156ed273ba517ba5b50e0c0c3652116581
Wes Scott <mcmanis000@gmail.com>
2018-10-30 16:45:08 -0700

    Added backup status indicator

--- Get-WSBData.ps1
+++ Get-WSBData.ps1
@@ -1,4 +1,6 @@
-﻿# Storing native command output as objects
+﻿$Threshold = 0
+
+# Storing native command output as objects
 $WBPolicy = Get-WBPolicy
 $WBSummary = Get-WBSummary
 
@@ -8,17 +10,25 @@ $WBSummary = Get-WBSummary
 [DateTime] $NextJob = $WBSummary.NextBackupTime
 
 # Determining the age of the last successful backup
-$LastJob = New-TimeSpan -Start ($LastSuccess) -End (Get-Date)
+$LastJob = New-TimeSpan -Start ($LastSuccess) -End ($ScriptRun)
 $Age = [math]::Round(($LastJob.Days) + (($LastJob.Hours) / 24), 2)
 
 # Getting the backup target type and scope of backup job
 $BackupType = (Get-WBBackupTarget -Policy $WBPolicy).TargetType
 $Scope = $WBPolicy.VolumesToBackup -join ", "
 
-# Getting all error logs for the time specified
-$WBErrors = (Get-WinEvent Microsoft-Windows-Backup |
-    Where-Object {($_.LevelDisplayName -like 'Error') -and ($_.TimeCreated -ge (get-date).AddDays(-1))})
+# Getting all error logs for the threshold specified
+$ErrorLogs = (Get-WinEvent Microsoft-Windows-Backup |
+    Where-Object {($_.LevelDisplayName -like 'Error') -and ($_.TimeCreated -ge ($ScriptRun).AddDays(-$Threshold))})
+[int] $ErrorCount = $ErrorLogs.Count
 
+If(($Age -gt $Threshold) -Or ($ErrorCount -gt 0)){
+    $BackupStatus = "Error"
+}
+
+Else{
+    $BackupStatus = "Normal"
+}
 
 ###############################
 # Output for testing purposes #
@@ -28,5 +38,9 @@ Write-Host "Next Job: "$NextJob
 Write-Host "Backup Type: "$BackupType
 Write-Host "Scope: $Scope"
 Write-Host "Backup Age: $Age Days"
+Write-Host "Error Count is: $ErrorCount"
+Write-Host "Backup Status is: $BackupStatus"
+
 Write-Host "`nThe following errors were found in the log: `n" 
-$WBErrors
+Write-Host "Error logs:"
+$ErrorLogs
