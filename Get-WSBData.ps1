7016cc04e4337362a127fe0c78e8bc22babdc881
Wes Scott <mcmanis000@gmail.com>
2018-10-30 16:02:32 -0700

    Optimizing current script

--- Get-WSBData.ps1
+++ Get-WSBData.ps1
@@ -2,27 +2,29 @@
 $WBPolicy = Get-WBPolicy
 $WBSummary = Get-WBSummary
 
-# Getting dates for script execution, last successful backup, and next scheduled backup
+# Getting datesfor script execution, last successful job, and next scheduled job
 [DateTime] $ScriptRun = Get-Date
 [DateTime] $LastSuccess = Get-Date $WBSummary.LastSuccessfulBackupTime
 [DateTime] $NextJob = Get-Date $WBSummary.NextBackupTime
-$LastJobRun = New-TimeSpan -Start ($LastSuccess) -End (Get-Date)
-$JobTimeFull = New-timeSpan -Start ($NextJob) -End ($LastSuccess).AddDays(1)
 
+# Determining the age of the last successful backup
+$LastJob = New-TimeSpan -Start ($LastSuccess) -End (Get-Date)
+$Age = [math]::Round(($LastJob.Days) + (($LastJob.Hours) / 24), 2)
+
+# Getting the backup target type and scope of backup job
 $BackupType = (Get-WBBackupTarget -Policy (Get-WBPolicy)).TargetType
-$JobTime = [math]::Round($JobTimeFull.TotalMinutes, 2)
 $Scope = (($WBPolicy | Select -Property VolumesToBackup | FT -HideTableHeaders | Out-String).trim()) -replace '[{}]', ''
-$Age = [math]::Round(($LastJobRun.days + ($LastJobRun.hours / 24)), 2)
+
+# Getting all error logs for the time specified
 $WBErrors = (Get-WinEvent Microsoft-Windows-Backup | Where-Object {($_.LevelDisplayName -like 'Error') -and ($_.TimeCreated -ge (get-date).AddDays(-1))})
 
 # Output for testing purposes
 
-Write-Host "`nScript run timestamp: "$ScriptRun
+Write-Host "`nScript Run Timestamp: "$ScriptRun
 Write-Host "Last Success: " $LastSuccess
 Write-Host "Next Job: "$NextJob
-Write-Host "Backup type: "$BackupType
-Write-Host "The job ran for: $JobTime minutes"
+Write-Host "Backup Type: "$BackupType
 Write-Host "Scope: $Scope"
-Write-Host "Backup Age: $Age"
+Write-Host "Backup Age: $Age Days"
 Write-Host "`nThe following errors were found in the log: `n" 
 $WBErrors
