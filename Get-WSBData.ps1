e1bdce35725c5f76e8074ecae3b8492ba375ef53
Wes Scott <mcmanis000@gmail.com>
2018-11-01 10:40:20 -0700

    Fix: Resolved an error with POwershell 2 compatibility
    Minor changes to the code to allow proper import of the Windows Server Backup snap in. Also removed some code used strictly for testing.

--- Get-WSBData.ps1
+++ Get-WSBData.ps1
@@ -1,10 +1,10 @@
 ﻿Function Get-WBStats {
     param(
-        $Threshold = 0
+        $Threshold = 1
     )
 
     # Added the Windows Server Backup module if not present
-    If((Get-Command Get-WBSummary).count -eq 0){
+    If((Get-Command Get-WBSummary).count -eq $Null){
         Add-PSSnapIn Windows.ServerBackup
     }
 
@@ -18,8 +18,12 @@
     [DateTime] $NextJob = $WBSummary.NextBackupTime
 
     # Determining the age of the last successful backup
-    $LastJob = New-TimeSpan -Start ($LastSuccess) -End ($ScriptRun)
+    $LastJob = New-TimeSpan -Start $LastSuccess -End $ScriptRun
     $Age = [math]::Round(($LastJob.Days) + (($LastJob.Hours) / 24), 2)
+
+    # Calculating the duration of the last backup job
+    $PreviousJob = Get-WBJob -Previous 1
+    $JobRunTime = New-TimeSpan -Start $PreviousJob.StartTime -End $PreviousJob.EndTime
 
     # Getting the backup target type and scope of backup job
     $BackupType = (Get-WBBackupTarget -Policy $WBPolicy).TargetType
@@ -28,30 +32,13 @@
     # Getting all error logs for the threshold specified
     $ErrorLogs = (Get-WinEvent Microsoft-Windows-Backup |
         Where-Object {($_.LevelDisplayName -like 'Error') -and ($_.TimeCreated -ge ($ScriptRun).AddDays(-$Threshold))})
-    [int] $ErrorCount = $ErrorLogs.Count
+    [int] $EventLogErrors = $ErrorLogs.Count
 
     # Setting status to 'error' if backup age is above threshold or error were detected in that time frame
     $BackupStatus = "Normal"
-    If(($Age -gt $Threshold) -Or ($ErrorCount -gt 0)){
+    If(($Age -gt $Threshold) -Or ($EventLogErrors -gt 0)){
         $BackupStatus = "Error"
     }
-
-
-    ###############################
-    # Output for testing purposes #
-    Write-Host "`nScript Run Timestamp: "$ScriptRun
-    Write-Host "Last Success: " $LastSuccess
-    Write-Host "Next Job: "$NextJob
-    Write-Host "Backup Type: "$BackupType
-    Write-Host "Scope: $Scope"
-    Write-Host "Backup Age: $Age Days"
-    Write-Host "Error Count is: $ErrorCount"
-    Write-Host "Backup Status is: $BackupStatus"
-
-    Write-Host "`nThe following errors were found in the log: `n" 
-    Write-Host "Error logs:"
-    $ErrorLogs
-
 }
 
-Get-WBStats -Threshold 1
+Get-WBStats -Threshold 3
