0f2bfa3f14c08e7a33f1752038fbd94879d69962
Wes Scott <mcmanis000@gmail.com>
2018-11-02 14:54:22 -0700

    Feat: Restructuring the script to be a PowerShell module collection; plus a few other things
    I rearranged the single script to be a Powershell module and added a way to output the Windows Server Backup data out to the CLI instead of file only.

---
+++ AutomateTools/AutomateTools.psm1
@@ -0,0 +1,71 @@
+﻿# A function used to output a hashtable as a csv file
+Function Out-PlainCSV {
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $FilePath,
+        [Parameter(Mandatory=$True)]
+        [hashtable] $Data,
+        $Delimiter = ","
+    )
+
+    # Building the string for output
+    [string] $CSVString = ""
+    $Data.GetEnumerator() | ForEach-Object{
+        $CSVString += "{0}{1}{2}{3}" -f $_.key, $Delimiter, $_.value, $Delimiter
+    }
+    
+    # Writing the string to a file
+    $CSVString.TrimEnd($Delimiter) | Out-File $FilePath
+}
+
+
+# A function used to output a hashtable as a xml file
+Function Out-PlainXML {
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $FilePath,
+        [string] $RootElement = "Data",
+        [Parameter(Mandatory=$True)]
+        [hashtable] $Data
+    )
+    # Special thanks to Roger Delph, from whom much of this XML generation code was borrowed:
+    # https://www.rogerdelph.com/creating-xml-documents-from-powershell/
+
+    # Create The Document
+    $XmlWriter = New-Object System.XMl.XmlTextWriter($FilePath,$Null)
+ 
+    # Set The Formatting
+    $xmlWriter.Formatting = "Indented"
+    $xmlWriter.Indentation = "4"
+ 
+    # Write the XML Decleration
+    $xmlWriter.WriteStartDocument()
+ 
+    # Set the XSL
+    $XSLPropText = "type='text/xsl' href='style.xsl'"
+    $xmlWriter.WriteProcessingInstruction("xml-stylesheet", $XSLPropText)
+ 
+    # Write Root Element
+    $xmlWriter.WriteStartElement("RootElement")
+ 
+    # Write the Document
+    $xmlWriter.WriteStartElement($RootElement)
+    $Data.GetEnumerator() | ForEach-Object{
+        $xmlWriter.WriteElementString($_.key, $_.value)
+    }
+    $xmlWriter.WriteEndElement | Out-Null
+ 
+    # Write Close Tag for Root Element
+    $xmlWriter.WriteEndElement | Out-Null
+ 
+    # End the XML Document
+    $xmlWriter.WriteEndDocument()
+ 
+    # Finish The Document
+    $xmlWriter.Finalize
+    $xmlWriter.Flush | Out-Null
+    $xmlWriter.Close()
+}
+
+# Adding supporting PowerShell functions
+. $PSScriptRoot\Get-WBStats.ps1
--- AutomateTools/Get-WBStats.ps1
+++ AutomateTools/Get-WBStats.ps1
@@ -1,79 +1,26 @@
-﻿# A function used to output a hashtable as a csv file
-Function Out-PlainCSV {
-    param(
-        [Parameter(Mandatory=$True)]
-        [string] $FilePath,
-        [Parameter(Mandatory=$True)]
-        [hashtable] $Data,
-        $Delimiter = ","
-    )
+﻿<#
+.SYNOPSIS
+Get-WBStats collects information from Windows Server Backup and outputs the data into the format specified.
+.DESCRIPTION
 
-    # Building the string for output
-    [string] $CSVString = ""
-    $Data.GetEnumerator() | ForEach-Object{
-        $CSVString += "{0}{1}{2}{3}" -f $_.key, $Delimiter, $_.value, $Delimiter
-    }
-    
-    # Writing the string to a file
-    $CSVString.TrimEnd($Delimiter) | Out-File $FilePath
-}
+.PARAMETER FilePath
 
+.PARAMETER OutputType
 
-# A function used to output a hashtable as a xml file
-Function Out-PlainXML {
-    param(
-        [Parameter(Mandatory=$True)]
-        [string] $FilePath,
-        [string] $RootElement = "Data",
-        [Parameter(Mandatory=$True)]
-        [hashtable] $Data
-    )
-    # Special thanks to Roger Delph, from whom much of this XML generation code was borrowed:
-    # https://www.rogerdelph.com/creating-xml-documents-from-powershell/
+.PARAMETER Delimiter
 
-    # Create The Document
-    $XmlWriter = New-Object System.XMl.XmlTextWriter($FilePath,$Null)
- 
-    # Set The Formatting
-    $xmlWriter.Formatting = "Indented"
-    $xmlWriter.Indentation = "4"
- 
-    # Write the XML Decleration
-    $xmlWriter.WriteStartDocument()
- 
-    # Set the XSL
-    $XSLPropText = "type='text/xsl' href='style.xsl'"
-    $xmlWriter.WriteProcessingInstruction("xml-stylesheet", $XSLPropText)
- 
-    # Write Root Element
-    $xmlWriter.WriteStartElement("RootElement")
- 
-    # Write the Document
-    $xmlWriter.WriteStartElement($RootElement)
-    $Data.GetEnumerator() | ForEach-Object{
-        $xmlWriter.WriteElementString($_.key, $_.value)
-    }
-    $xmlWriter.WriteEndElement | Out-Null
- 
-    # Write Close Tag for Root Element
-    $xmlWriter.WriteEndElement | Out-Null
- 
-    # End the XML Document
-    $xmlWriter.WriteEndDocument()
- 
-    # Finish The Document
-    $xmlWriter.Finalize
-    $xmlWriter.Flush | Out-Null
-    $xmlWriter.Close()
-}
+.PARAMETER Threshold
 
+.EXAMPLE
 
-# A function used to collect Windows Server Backup stats and output them in the specified format
+.LINK
+https://github.com/WesScott000/AutomateTools
+#>
+
 Function Get-WBStats {
     param(
-        [Parameter(Mandatory=$True)]
-        [string] $FilePath,
-        [ValidateSet("Xml", "CliXml", "Csv")]
+        [string] $FilePath = "C:\WBStats.xml",
+        [ValidateSet("Xml", "CliXml", "Csv", "Cli")]
         [string] $OutputType = "Xml",
         $Delimiter = ",",
         [int] $Threshold = 1
@@ -136,16 +83,10 @@ Function Get-WBStats {
     ElseIf($OutputType -eq "Csv"){
         Out-PlainCsv -FilePath $FilePath -Delimiter $Delimiter -Data $Data
     }
+    ElseIf($OutputType -eq "Cli"){
+        $Data
+    }
     Else{
         Out-PlainXML -FilePath $FilePath -Data $Data
     }
-    
 }
-
-# Output Testing #
-
-# Get-WBStats -FilePath "C:\test.xml" -OutputType csv -Delimiter " | " -Threshold 1
-
-# Get-WBStats -FilePath "C:\test.xml" -OutputType clixml -Threshold 10
-
-# Get-WBStats -FilePath "C:\test.xml"
