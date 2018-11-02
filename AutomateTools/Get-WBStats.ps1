90b7115b63121889023e7ddb8971db735bd3c238
Wes Scott <mcmanis000@gmail.com>
2018-11-02 16:46:51 -0700

    Feat: Added built in PowerShell help system

--- AutomateTools/Get-WBStats.ps1
+++ AutomateTools/Get-WBStats.ps1
@@ -1,18 +1,32 @@
 ﻿<#
 .SYNOPSIS
-Get-WBStats collects information from Windows Server Backup and outputs the data into the format specified.
+Get-WBStats collects information from Windows Server Backup and outputs the data as a file.
 .DESCRIPTION
-
+This command exports Windows Server Backup data as a file. Therefore, you must have the Windows Server Backup feature 
+installed. Additionally, you must also save the file to a valid file location and specify the file name and type.
+Additionally, you may also optionally output the collected data to the CLI window.
 .PARAMETER FilePath
-
+Accepts the full path of the desired output file. Must include full path, filename, and extension.
 .PARAMETER OutputType
-
+Accepts 'xml', 'clixml', or 'csv' depending on the desired output result. Set to 'xml' by default.
 .PARAMETER Delimiter
-
+Accepts and delimiter to be used with 'csv' output. Set to ',' by default.
 .PARAMETER Threshold
-
+Sets the threshold used to check event logs for backup errors. Accepts any integer from 0 to 31.
+.PARAMETER CliOutput
+Accepts $True or $False. Setting to $True will output data results to the CLI.
 .EXAMPLE
-
+This is the most basic form of the command. The results will be xml data stored in the file as specified:
+    Get-WBStats -FilePath "C:\Logs\WBStatsResults.xml"
+.EXAMPLE
+This command will run as the previous example but also output the data to the CLI on completion:
+    Get-WBStats -FilePath "C:\Logs\WBStatsResults.xml" -CliOutput $True
+.EXAMPLE
+This command will output a file that contains clixml data, checking the Windows Server Backup event logs for errors during the last 14 days.
+    Get-WBStats -FilePath "C:\Logs\WBStatsResults.xml" -OutputType clixml -Threshold 14
+.EXAMPLE
+This command will output a file that contains comma seperated values and replace the commas with a vertical pipe delimiter:
+    Get-WBStats -FilePath "C:\Logs\WBStatsResults.csv" -OutputType csv -Delimiter " | "
 .LINK
 https://github.com/WesScott000/AutomateTools
 #>
@@ -20,7 +34,6 @@ https://github.com/WesScott000/AutomateTools
 Function Get-WBStats {
     param(
         [Parameter(Mandatory=$True)]
-        [ValidatePattern("[a-zA-Z]\:\\.{1,}[.][cxtCXT][smxSMX][vltVLT]")]
         [string] $FilePath,
         [ValidateSet("Xml", "CliXml", "Csv")]
         [string] $OutputType = "Xml",
