496e36fc2464a093e511b8d7721d56aae2dbe7be
Wes Scott <mcmanis000@gmail.com>
2018-11-06 14:58:44 -0800

    Feat: Updated help

--- AutomateTools/Backup-NoahDatabase.ps1
+++ AutomateTools/Backup-NoahDatabase.ps1
@@ -1,20 +1,10 @@
 ﻿<#
 .SYNOPSIS
-Remove-NoahBackups removes backup files created by the Noah 4 system.
+Backup-NoahDatabase makes a copy of the primary Noah database.
 .DESCRIPTION
-This command removes backup files from the default Noah backup location. You can specify an alternate path to the backup files if they are not in a default directory.
-Optionally, you can specify the age of the files you wish to remove and retain a minimum number of backup copies, despite the age.
-.PARAMETER Path
-Specifies the path to the Noah 4 backups, if they are not in the default location of: C:\ProgramData\Himsa\Noah\Backup\Database
-.PARAMETER Threshold
-Specifies the age in days of the backups that you wish to remove. The deafult value is 90 days.
-.PARAMETER Keep
-Specifies the number of backup files you wish to keep regardless of the age. The default value is 3.
-.PARAMETER CliOutput
-Set this value to $True if you would like to see the output from the command.
-.EXAMPLE
-Remove-NoahBackups -Threshold 15 -Keep 1 -CliOutput $True
-This command will remove all backups older than 15 days, keep 1 file at a minimum disregarding it's age, and output the result of the command.
+This command stops the Noah services and copies the default database to the default location of "C:\AutomateTools\Backups\ in a time stamped folder.
+.PARAMETER Destination
+Specifies an alternative path to save the backup database.
 .LINK
 https://github.com/WesScott000/AutomateTools
 #>
