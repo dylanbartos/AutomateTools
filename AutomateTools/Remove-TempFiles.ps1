6bab3519b2138b2f53ff95c407a0b8c2aa355017
Wes Scott <mcmanis000@gmail.com>
2018-11-13 14:09:18 -0800

    Feat: removed counter

--- AutomateTools/Remove-TempFiles.ps1
+++ AutomateTools/Remove-TempFiles.ps1
@@ -5,12 +5,16 @@
         [Parameter(Mandatory=$True)]
         [string] $FileType,
         [int] $Depth = 0,
-        [int] $Age = 30
+        [int] $Age = 30,
+        [int] $SpaceCleaned = 0
     )
 
     [datetime] $ThresholdDate = (Get-Date).AddDays(-$Age)
-    $r = Get-ChildItem -Path $Path -Depth $Depth | Where-Object { ($_.name -match "\....\b") -and ($_.name -like $FileType) -and ($_.LastWriteTime -lt $ThresholdDate) }
-    $r | Remove-Item -Force -ErrorAction Ignore
+    $results = Get-ChildItem -Path $Path -Depth $Depth -ErrorAction Ignore | Where-Object { ($_.name -match "\....\b") -and ($_.name -like $FileType) -and ($_.LastWriteTime -lt $ThresholdDate) }
+
+    ForEach($result in $results){
+        $Result | Remove-Item -Force -ErrorAction Ignore
+    }
  }
 
 Remove-TempFiles -Path ($env:windir + "\temp\") -FileType "*.*" -Depth 1
@@ -18,7 +22,7 @@ Remove-TempFiles -Path ($env:windir + "\system32\wbem\logs\") -FileType "*.*" -A
 Remove-TempFiles -Path ($env:windir + "\system32\wbem\logs\") -FileType "*.*" -Depth 1
 Remove-TempFiles -Path ($env:windir + "\system32\logfiles\") -FileType "*.*" -Depth 1
 Remove-TempFiles -Path ($env:windir + "\debug\") -FileType "*.*" -Depth 1 -Age 0
-Remove-TempFiles -Path ($env:windir + "\Logs\CBS\") -FileType "*.log" -Age 15
+Remove-TempFiles -Path ($env:windir + "\Logs\CBS\") -FileType "*.log" -Age 14
 Remove-TempFiles -Path ($env:systemdrive + "\users\*\AppData\Local\Temp") -FileType "*.*" -Depth 1 -Age 0
 Remove-TempFiles -Path ($env:systemdrive + "\users\*\AppData\Local\Temp") -FileType "*.*" -Depth 1 -Age 0
 Remove-TempFiles -Path ($env:systemdrive + "\users\*\Downloads\") -FileType "*.scl" -Age 0
