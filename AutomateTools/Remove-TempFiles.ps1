a9facef5d8eff70f387c6e0de5f22ef27ab288f6
Wes Scott <mcmanis000@gmail.com>
2018-11-13 13:31:51 -0800

    Feat: Completed temp file cleanup script

--- AutomateTools/Remove-TempFiles.ps1
+++ AutomateTools/Remove-TempFiles.ps1
@@ -1,13 +1,25 @@
-﻿$FileInfo = Get-ChildItem 'C:\Windows\Logs\CBS'
-$SizeInGBs = 4
-$TotalSize = 0
+﻿Function Remove-TempFiles {
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $Path,
+        [Parameter(Mandatory=$True)]
+        [string] $FileType,
+        [int] $Depth = 0,
+        [int] $Age = 30
+    )
 
-ForEach($File in $FileInfo){
-    If($File -like 'CbsPersist*.cab'){
-        $TotalSize = ($File.Length / 10000000)
-    }
-}
+    [datetime] $ThresholdDate = (Get-Date).AddDays(-$Age)
+    $r = Get-ChildItem -Path $Path -Depth $Depth | Where-Object { ($_.name -match "\....\b") -and ($_.name -like $FileType) -and ($_.LastWriteTime -lt $ThresholdDate) }
+    $r | Remove-Item -Force -ErrorAction Ignore
+ }
 
-If($TotalSize -gt $SizeInGBs){
-    Remove-Item -Path 'C:\Windows\Logs\CBS\CbsPersist*.cab' -Force -ErrorAction Ignore
-}
+Remove-TempFiles -Path ($env:windir + "\temp\") -FileType "*.*" -Depth 1
+Remove-TempFiles -Path ($env:windir + "\system32\wbem\logs\") -FileType "*.*" -Age 0
+Remove-TempFiles -Path ($env:windir + "\system32\wbem\logs\") -FileType "*.*" -Depth 1
+Remove-TempFiles -Path ($env:windir + "\system32\logfiles\") -FileType "*.*" -Depth 1
+Remove-TempFiles -Path ($env:windir + "\debug\") -FileType "*.*" -Depth 1 -Age 0
+Remove-TempFiles -Path ($env:windir + "\Logs\CBS\") -FileType "*.log" -Age 15
+Remove-TempFiles -Path ($env:systemdrive + "\users\*\AppData\Local\Temp") -FileType "*.*" -Depth 1 -Age 0
+Remove-TempFiles -Path ($env:systemdrive + "\users\*\AppData\Local\Temp") -FileType "*.*" -Depth 1 -Age 0
+Remove-TempFiles -Path ($env:systemdrive + "\users\*\Downloads\") -FileType "*.scl" -Age 0
+Remove-TempFiles -Path ($env:ProgramData + "\Himsa\Noah\Backup\Database") -FileType "*.*" -Depth 2 -Age 90
