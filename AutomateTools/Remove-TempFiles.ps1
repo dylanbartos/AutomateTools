99e73876d396d647479ae3b7200a2b1ad291848f
Wes Scott <mcmanis000@gmail.com>
2018-11-21 12:16:58 -0800

    Feat: Simplified Remove-TempFiles function

--- AutomateTools/Remove-TempFiles.ps1
+++ AutomateTools/Remove-TempFiles.ps1
@@ -1,75 +1,38 @@
-﻿ Function Remove-TempFiles{
+﻿ Function Remove-TempFiles {
     param(
-        [Parameter(Mandatory=$True)]
         $Path,
+        [string] $FileType = "*.*",
+        [bool] $Recurse = $False,
+        [int] $Age = 0,
         [Parameter(Mandatory=$True)]
-        $FileType,
-        [ValidateRange(0,3)]
-        $Depth = 1,
-        $Age = 30
+        $ValidationString
     )
 
     If(!(Test-Path $Path)){
         Exit
     }
 
-    [datetime] $ThresholdDate = (Get-Date).AddDays(-$Age)
-    $OriginPath = Get-Item $Path
-    $SpaceFreed = 0
-    $L1Paths = @()
-    $L2Paths = @()
-    $L3Paths = @()
+    $Path = Get-Item $Path
+    [datetime] $TDate = (Get-Date).AddDays(-$Age)
 
-    If($Depth -gt 0){
-        $FoundPaths = @($OriginPath)
-        $L1Paths += (Get-ChildItem $OriginPath.FullName | Where-Object {$_.Attributes -eq "Directory"})
-        $FoundPaths += $L1Paths
+    If($Recurse -eq $True){
+        $Contents = Get-ChildItem -Path $Path.FullName -Recurse | Where-Object {($_.Extension -like $FileType)} 
+    }
 
-        If(($Depth -gt 1) -and ($L1Paths)){
-            ForEach($L1Path in $L1Paths){
-                $L2Paths += Get-ChildItem $L1Path.FullName | Where-Object {$_.Attributes -eq "Directory"}
-            }
-            $FoundPaths += $L2Paths
+    Else{
+        $Contents = Get-ChildItem -Path $Path.FullName | Where-Object {($_.Extension -like $FileType)}
+    }
 
-            If(($Depth -gt 2) -and ($L2Paths)){
-                ForEach($L2Path in $L2Paths){
-                    $L3Paths += Get-ChildItem $L2Path.FullName | Where-Object {$_.Attributes -eq "Directory"}
-                }
-                $FoundPaths += $L3Paths
-
-            }
+    ForEach($c in $Contents){
+        If($c.fullname -like $ValidationString){
+            $c | Remove-Item
         }
     }
 
-    $DetectedFiles = @()
-    ForEach($P in $FoundPaths){
-        $FoundFile = Get-ChildItem -Path $P.FullName | Where-Object {($_.Attributes -ne "Directory") -and ($_.LastWriteTime -lt $ThresholdDate) -and ($_.Extension -like $FileType)}
-        $DetectedFiles += $FoundFile
-    }
-
-
-
-# Test Output #
-$Break = "`n------------------------`n"
-$Break | Add-Content "C:\TestResults.txt"
-$OriginPath.fullname | Add-Content "C:\TestResults.txt"
-$L1Paths.fullname | Add-Content "C:\TestResults.txt"
-$L2Paths.fullname | Add-Content "C:\TestResults.txt"
-$L3Paths.fullname | Add-Content "C:\TestResults.txt"
-$DetectedFiles.FullName | Add-Content "C:\TestResults.txt"
-$Break | Add-Content "C:\TestResults.txt"
 }
 
-If(Test-Path "C:\TestResults.txt"){
-    Remove-Item "C:\TestResults.txt"
-    New-Item -ItemType File "C:\TestResults.txt"
-}
-
-Remove-TempFiles -Path ($env:windir + "\temp\") -FileType "*.*" -Depth 1
-Remove-TempFiles -Path ($env:windir + "\system32\wbem\logs\") -FileType "*.*" -Depth 1 -Age 0
-Remove-TempFiles -Path ($env:windir + "\system32\logfiles\") -FileType "*.*" -Depth 1
-Remove-TempFiles -Path ($env:windir + "\debug\") -FileType "*.*" -Depth 1 -Age 0
-Remove-TempFiles -Path ($env:windir + "\Logs\CBS\") -FileType "*.log" -Age 14
-Remove-TempFiles -Path ($env:systemdrive + "\users\*\AppData\Local\Temp\") -FileType "*.*" -Depth 1 -Age 0
-Remove-TempFiles -Path ($env:systemdrive + "\users\*\Downloads\") -FileType "*.scl" -Age 0
-Remove-TempFiles -Path ($env:ProgramData + "\Himsa\Noah\Backup\Database\") -FileType "*.*" -Depth 2 -Age 90
+# List of specific temp locations
+Remove-TempFiles -Path ("C:\Windows\temp\") -Recurse $True -ValidationString "C:\Windows\temp\*"
+Remove-TempFiles -Path ("C:\Windows\Logs\CBS\") -FileType "*.log" -Age 14 -ValidationString "C:\Windows\Logs\CBS\*.log"
+Remove-TempFiles -Path ("C:\users\*\AppData\Local\Temp\") -FileType "*.*" -ValidationString "C:\users\*\AppData\Local\Temp\*"
+Remove-TempFiles -Path ("C:\users\*\Downloads\") -FileType "*.scl" -ValidationString "C:\users\*\Downloads\*.scl"
