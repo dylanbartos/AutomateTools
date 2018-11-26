d2519fa9a5c0baae0f84003febf147f211416d53
Wes Scott <mcmanis000@gmail.com>
2018-11-26 15:50:04 -0800

    Info: Updated help menus on each function

--- AutomateTools/Remove-MiscTempFiles.ps1
+++ AutomateTools/Remove-MiscTempFiles.ps1
@@ -1,47 +1,78 @@
-﻿Function Remove-MiscTempFiles{
+﻿<#
+.SYNOPSIS
+Remove-MiscTempFiles removes files of the type and location specified from locations.
+.DESCRIPTION
+This tool should not be used in production as it's too powerful and there are a few strange bugs with enumerating the sub directories.
+.EXAMPLE
+This will remove all .log files in the C:\Windows\Logs\CBS\ folder that are older than 13 days and does not recurse through subdirectories.
+Each file deleted must have a full file path that matches "C:\Windows\Logs\CBS\*.log".
+    Remove-Files -Path "C:\Windows\Logs\CBS\" -FileType "*.log" -Age 14 -ValidationString "C:\Windows\Logs\CBS\*.log"
+.EXAMPLE
+This will iterate through all user downloads folders and remove all .scl files greater than or equal to one day in age.
+Each file deleted must have a full file path that matches "C:\users\*\Downloads\*.scl".
+    Remove-Files -Path "C:\users\*\Downloads\" -FileType "*.scl" -Age 1 -ValidationString "C:\users\*\Downloads\*.scl"
+.EXAMPLE
+This will remove all files in all subdirectories for the location "C:\Temp":
+    Remove-Files -Path "C:\Temp\" -FileType "*.*" -Recurse $True -ValidationString "C:\Temp\*.*"
+.LINK
+https://github.com/WesScott000/AutomateTools
+#>
+Function Remove-MiscTempFiles {
+    param(
+        [Parameter(Mandatory=$True)]
+        $Path,
+        [Parameter(Mandatory=$True)]
+        [string] $FileType,
+        [bool] $Recurse = $False,
+        [int] $Age = 0,
+        [Parameter(Mandatory=$True)]
+        $ValidationString
+    )
 
-    Function Remove-Files {
-        param(
-            [Parameter(Mandatory=$True)]
-            $Path,
-            [Parameter(Mandatory=$True)]
-            [string] $FileType,
-            [bool] $Recurse = $False,
-            [int] $Age = 0,
-            [Parameter(Mandatory=$True)]
-            $ValidationString
-        )
-
-        If(!(Test-Path $Path)){
-            Exit
-        }
-
-        $Path = Get-Item $Path
-        [datetime] $TDate = (Get-Date).AddDays(-$Age)
-
-        If($Recurse -eq $True){
-            $Contents = Get-ChildItem -Path $Path.FullName -Recurse | Where-Object {($_.Extension -like $FileType)} 
-        }
-
-        Else{
-            $Contents = Get-ChildItem -Path $Path.FullName | Where-Object {($_.Extension -like $FileType)}
-        }
-
-        ForEach($c in $Contents){
-            If($c.fullname -like $ValidationString){
-                # File removal disabled pending more testing.
-                $c #| Remove-Item
-            }
-        }
+    # Exit if path to location does not exist.
+    If(!(Test-Path $Path)){
+        Exit
     }
 
-# Removal of CBS Log files.
-Remove-Files -Path ("C:\Windows\Logs\CBS\") -FileType "*.log" -Age 14 -ValidationString "C:\Windows\Logs\CBS\*.log"
-# Removal of .scl files left behind by Sycle Noah Sync.
-Remove-Files -Path ("C:\users\*\Downloads\") -FileType "*.scl" -Age 1 -ValidationString "C:\users\*\Downloads\*.scl"
+    # Getting path and date objects.
+    $Path = Get-Item $Path
+    [datetime] $TDate = (Get-Date).AddDays(-$Age)
+
+    # Getting contents if recursion is specified.
+    If($Recurse -eq $True){
+        $Contents = Get-ChildItem -Path $Path.FullName -Recurse | Where-Object {($_.Extension -like $FileType)} 
+    }
+
+    # Getting contents where recursion is not required.
+    Else{
+        $Contents = Get-ChildItem -Path $Path.FullName | Where-Object {($_.Extension -like $FileType)}
+    }
+
+    # Enumerating contents and removing only those that share the same full path pattern specified.
+    ForEach($c in $Contents){
+        If($c.fullname -like $ValidationString){
+        # Only outputs file names for now; pending further testing.
+            $c #| Remove-Item
+        }
+    }
 }
 
 
+<#
+.SYNOPSIS
+Open-DiskCleanSetup simply opens the cleanmgr.exe configuration and saves the values under configuration number 7.
+The configuration number can be modified.
+.PARAMETER CfgNum
+Allows you to change the configuration settings to save as a different value.
+.EXAMPLE
+Opens the configuration settings at the default value of 7:
+    Open-DiskCleanSetup
+.EXAMPLE 
+Opens the configuration settings as a user specified value of 3:
+    Open-DiskCleanSetup -CfgNum 3
+.LINK
+https://github.com/WesScott000/AutomateTools
+#>
 Function Open-DiskCleanSetup{
     param(
         [int]$CfgNum = 7
@@ -80,6 +111,21 @@ Function Open-DiskCleanSetup{
 }
 
 
+<#
+.SYNOPSIS
+Run-DiskCleanup runs cleanmgr.exe at the default preconfigured settings of 7 and returns a timestamp.
+The configuration number can be modified.
+.PARAMETER CfgNum
+Allows you to change the configuration settings to save as a different value.
+.EXAMPLE
+Runs cleanmgr.exe and uses the preconfigured settings assigned to configuration 7:
+    Run-DiskCleanup
+.EXAMPLE 
+Runs cleanmgr.exe and uses the preconfigured settings assigned to configuration 3:
+    Run-DiskCleanup -CfgNum 3
+.LINK
+https://github.com/WesScott000/AutomateTools
+#>
 Function Run-DiskCleanup{
     param(
         [int]$CfgNum = 7
