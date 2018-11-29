56f360ad3e39bb0ae808fcf12f94fd37b69fa90d
Wes Scott <mcmanis000@gmail.com>
2018-11-29 11:01:34 -0800

    Feat: Removed in code comments and many empty lines to slim the code as much as possible

--- AutomateTools/AutomateTools.psm1
+++ AutomateTools/AutomateTools.psm1
@@ -1,5 +1,4 @@
-﻿# A function used to output a hashtable as a csv file
-Function Out-PlainCSV {
+﻿Function Out-PlainCSV {
     param(
         [Parameter(Mandatory=$True)]
         [string] $FilePath,
@@ -7,19 +6,13 @@ Function Out-PlainCSV {
         [hashtable] $Data,
         $Delimiter = ","
     )
-
-    # Building the string for output
     [string] $CSVString = ""
     $Data.GetEnumerator() | ForEach-Object{
         $CSVString += "{0}{1}{2}{3}" -f $_.key, $Delimiter, $_.value, $Delimiter
     }
-    
-    # Writing the string to a file
     $CSVString.TrimEnd($Delimiter) | Out-File $FilePath
 }
 
-
-# A function used to output a hashtable as a xml file
 Function Out-PlainXML {
     param(
         [Parameter(Mandatory=$True)]
@@ -28,59 +21,33 @@ Function Out-PlainXML {
         [Parameter(Mandatory=$True)]
         [hashtable] $Data
     )
-    # Special thanks to Roger Delph, from whom much of this XML generation code was borrowed:
-    # https://www.rogerdelph.com/creating-xml-documents-from-powershell/
-
-    # Create The Document
     $XmlWriter = New-Object System.XMl.XmlTextWriter($FilePath,$Null)
- 
-    # Set The Formatting
     $xmlWriter.Formatting = "Indented"
     $xmlWriter.Indentation = "4"
- 
-    # Write the XML Decleration
     $xmlWriter.WriteStartDocument()
- 
-    # Set the XSL
     $XSLPropText = "type='text/xsl' href='style.xsl'"
     $xmlWriter.WriteProcessingInstruction("xml-stylesheet", $XSLPropText)
- 
-    # Write Root Element
     $xmlWriter.WriteStartElement("RootElement")
- 
-    # Write the Document
     $xmlWriter.WriteStartElement($RootElement)
     $Data.GetEnumerator() | ForEach-Object{
         $xmlWriter.WriteElementString($_.key, $_.value)
     }
     $xmlWriter.WriteEndElement | Out-Null
- 
-    # Write Close Tag for Root Element
     $xmlWriter.WriteEndElement | Out-Null
- 
-    # End the XML Document
     $xmlWriter.WriteEndDocument()
- 
-    # Finish The Document
     $xmlWriter.Finalize
     $xmlWriter.Flush | Out-Null
     $xmlWriter.Close()
 }
 
-
-# A function to test and build a given file path
 Function Push-FileStructure{
     param(
         [Parameter(Mandatory=$True)]
         [string] $Path
     )
-
-    # Adds the trailing slash if left out
     If(!($Path.EndsWith('\'))){
         $Path += '\'
     }
-
-    # Test each level of the file path and creates it if it doesn't exist
     $Parts = $Path.split('\')
     ForEach($Part in $Parts){
         $RebuiltPath += ($Part + '\')
@@ -90,7 +57,6 @@ Function Push-FileStructure{
     }
 }
 
-# Adding supporting PowerShell functions
 . $PSScriptRoot\Get-WBStats.ps1
 . $PSScriptRoot\Get-NoahVersion.ps1
 . $PSScriptRoot\Get-FirewallStatus.ps1
