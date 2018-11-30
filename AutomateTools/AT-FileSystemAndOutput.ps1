b03e02f62d08850ae4a1db675fd1ba3771dd5b4c
Wes Scott <mcmanis000@gmail.com>
2018-11-29 16:33:05 -0800

    Moved functions to dedicated file; simplfied Test-AutomateTools logic

---
+++ AutomateTools/AT-FileSystemAndOutput.ps1
@@ -0,0 +1,70 @@
+﻿ Function Out-PlainCSV {
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $FilePath,
+        [Parameter(Mandatory=$True)]
+        [hashtable] $Data,
+        $Delimiter = ","
+    )
+
+    [string] $CSVString = ""
+    $Data.GetEnumerator() | ForEach-Object{
+        $CSVString += "{0}{1}{2}{3}" -f $_.key, $Delimiter, $_.value, $Delimiter
+    }
+    
+    $CSVString.TrimEnd($Delimiter) | Out-File $FilePath
+}
+
+
+Function Out-PlainXML {
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $FilePath,
+        [string] $RootElement = "Data",
+        [Parameter(Mandatory=$True)]
+        [hashtable] $Data
+    )
+
+    $XmlWriter = New-Object System.XMl.XmlTextWriter($FilePath,$Null)
+ 
+    $xmlWriter.Formatting = "Indented"
+    $xmlWriter.Indentation = "4"
+ 
+    $xmlWriter.WriteStartDocument()
+    $XSLPropText = "type='text/xsl' href='style.xsl'"
+    $xmlWriter.WriteProcessingInstruction("xml-stylesheet", $XSLPropText)
+    $xmlWriter.WriteStartElement("RootElement")
+ 
+    $xmlWriter.WriteStartElement($RootElement)
+    $Data.GetEnumerator() | ForEach-Object{
+        $xmlWriter.WriteElementString($_.key, $_.value)
+    }
+    $xmlWriter.WriteEndElement | Out-Null
+    $xmlWriter.WriteEndElement | Out-Null
+ 
+    $xmlWriter.WriteEndDocument()
+ 
+    $xmlWriter.Finalize
+    $xmlWriter.Flush | Out-Null
+    $xmlWriter.Close()
+}
+
+
+Function Push-FileStructure{
+    param(
+        [Parameter(Mandatory=$True)]
+        [string] $Path
+    )
+
+    If(!($Path.EndsWith('\'))){
+        $Path += '\'
+    }
+
+    $Parts = $Path.split('\')
+    ForEach($Part in $Parts){
+        $RebuiltPath += ($Part + '\')
+        If(!(Test-Path $RebuiltPath)){
+            New-Item -ItemType Directory -Path $RebuiltPath
+        }
+    }
+}
--- AutomateTools/AutomateTools.psm1
+++ AutomateTools/AutomateTools.psm1
@@ -1,106 +1,28 @@
 ﻿Function Test-AutomateTools{
-    param(
-        [bool] $EchoOnly = $False
-    )
 
-    If($EchoOnly -eq $True){
-        Return "True"
+    [bool] $CatResult = $False
+
+    If(Test-Path "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"){
+        [string] $R = cat "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
+        If($R -like "Import-Module C:\AutomateTools\AutomateTools.psm1"){
+            $CatResult = $True
+        }
     }
 
     Else{
-        [bool] $CatResult = $False
-        If(Test-Path "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"){
-            [string] $R = cat "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
-            If($R -like "Import-Module C:\AutomateTools\AutomateTools.psm1"){
-                $CatResult = $True
-            }
-        }
+        Return "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 is not present."
+        Exit
+    }
 
-        Else{
-            Return "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 is not present."
-            Exit
-        }
-
-        Return $CatResult
+    If($CatResult -eq $True){
+        Return "Automate Tools Base Configuration: Successful!"
     }
 }
-                
-
-Function Out-PlainCSV {
-    param(
-        [Parameter(Mandatory=$True)]
-        [string] $FilePath,
-        [Parameter(Mandatory=$True)]
-        [hashtable] $Data,
-        $Delimiter = ","
-    )
-
-    [string] $CSVString = ""
-    $Data.GetEnumerator() | ForEach-Object{
-        $CSVString += "{0}{1}{2}{3}" -f $_.key, $Delimiter, $_.value, $Delimiter
-    }
-    
-    $CSVString.TrimEnd($Delimiter) | Out-File $FilePath
-}
-
-
-Function Out-PlainXML {
-    param(
-        [Parameter(Mandatory=$True)]
-        [string] $FilePath,
-        [string] $RootElement = "Data",
-        [Parameter(Mandatory=$True)]
-        [hashtable] $Data
-    )
-
-    $XmlWriter = New-Object System.XMl.XmlTextWriter($FilePath,$Null)
  
-    $xmlWriter.Formatting = "Indented"
-    $xmlWriter.Indentation = "4"
- 
-    $xmlWriter.WriteStartDocument()
- 
-    $XSLPropText = "type='text/xsl' href='style.xsl'"
-    $xmlWriter.WriteProcessingInstruction("xml-stylesheet", $XSLPropText)
- 
-    $xmlWriter.WriteStartElement("RootElement")
- 
-    $xmlWriter.WriteStartElement($RootElement)
-    $Data.GetEnumerator() | ForEach-Object{
-        $xmlWriter.WriteElementString($_.key, $_.value)
-    }
-    $xmlWriter.WriteEndElement | Out-Null
- 
-    $xmlWriter.WriteEndElement | Out-Null
- 
-    $xmlWriter.WriteEndDocument()
- 
-    $xmlWriter.Finalize
-    $xmlWriter.Flush | Out-Null
-    $xmlWriter.Close()
-}
+# Updated List
+. $PSScriptRoot\AT-FileSystemAndOutput.ps1
 
-
-Function Push-FileStructure{
-    param(
-        [Parameter(Mandatory=$True)]
-        [string] $Path
-    )
-
-    If(!($Path.EndsWith('\'))){
-        $Path += '\'
-    }
-
-    $Parts = $Path.split('\')
-    ForEach($Part in $Parts){
-        $RebuiltPath += ($Part + '\')
-        If(!(Test-Path $RebuiltPath)){
-            New-Item -ItemType Directory -Path $RebuiltPath
-        }
-    }
-}
-
-
+# Old List
 . $PSScriptRoot\Get-WBStats.ps1
 . $PSScriptRoot\Get-NoahVersion.ps1
 . $PSScriptRoot\Get-FirewallStatus.ps1
