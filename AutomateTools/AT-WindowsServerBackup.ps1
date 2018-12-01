d00560df3a78650becaa6ad339a2d0f97c7d535f
Wes Scott <mcmanis000@gmail.com>
2018-11-30 16:23:42 -0800

    Removed comments from AT-WindowsServerBackup.ps1

--- AutomateTools/AT-WindowsServerBackup.ps1
+++ AutomateTools/AT-WindowsServerBackup.ps1
@@ -43,45 +43,34 @@ Function Get-WBStats {
         [bool] $CliOutput = $False
     )
 
-    # Added the Windows Server Backup module if not present
-	#-eq 0 = PSv3+
-	#-eq $null = PSv2
     If(((Get-Command Get-WBSummary*).count -eq 0) -Or ((Get-Command Get-WBSUmmary*) -eq $Null)){
         Add-PSSnapIn Windows.ServerBackup
     }
 
-    # Storing native command output as objects
     $WBPolicy = Get-WBPolicy
     $WBSummary = Get-WBSummary
 
-    # Getting datesfor script execution, last successful job, and next scheduled job
     [DateTime] $ScriptRun = Get-Date
     [DateTime] $LastSuccess = $WBSummary.LastSuccessfulBackupTime
     [DateTime] $NextJob = $WBSummary.NextBackupTime
 
-    # Determining the age of the last successful backup
     $LastJob = New-TimeSpan -Start $LastSuccess -End $ScriptRun
     $Age = [math]::Round(($LastJob.Days) + (($LastJob.Hours) / 24), 2)
 
-    # Calculating the duration of the last backup job
     $PreviousJob = Get-WBJob -Previous 1
     $JobRunTime = New-TimeSpan -Start $PreviousJob.StartTime -End $PreviousJob.EndTime
 
-    # Getting the backup target type and scope of backup job
     $BackupType = (Get-WBBackupTarget -Policy $WBPolicy).TargetType
     $Scope = $WBPolicy.VolumesToBackup -join ", "
 
-    # Getting all error logs for the threshold specified
     $ErrorLogs = (Get-WinEvent Microsoft-Windows-Backup |
         Where-Object {($_.LevelDisplayName -like 'Error') -and ($_.TimeCreated -ge ($ScriptRun).AddDays(-$Threshold))})
 
-    # Setting status to 'error' if backup age is above threshold or error were detected in that time frame
     $BackupStatus = "Normal"
     If(($Age -gt $Threshold) -Or ($ErrorLogs.count -gt 0)){
         $BackupStatus = "Error"
     }
 
-    # Setting data in a hash table for output
     $Data = @{
         ScriptRun = $ScriptRun
         LastSuccess = $LastSuccess
@@ -95,26 +84,21 @@ Function Get-WBStats {
     }
     
     Try {
-        # Output data to XML format, determined by user input
         If($OutputType -eq "CliXml"){
             $Data | Export-Clixml -Path $FilePath
         }
-        # Output data to CSV format, determined by user input
         ElseIf($OutputType -eq "Csv"){
             Out-PlainCsv -FilePath $FilePath -Delimiter $Delimiter -Data $Data
         }
-        # Default output
         Else{
             Out-PlainXML -FilePath $FilePath -Data $Data
         }
-        # Output result in the event of an output error
     } Catch {
         Write-Host "`n[!] There was a problem saving the output file. It's possible that:
         - The file path does not exit.
         - You don't have permissions to write to the file path."
     }
 
-    # Output data to console if flagged for CLI output
     If($CliOutput -eq $True){
         $Data
     }
