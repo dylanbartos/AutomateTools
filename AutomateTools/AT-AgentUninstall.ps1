8ab51e20381308be5f061c5815213136047cfc3e
Dylan Bartos <mrdboutdoors@gmail.com>
2020-04-21 10:34:44 -0700

    Removed file hashing

--- AutomateTools/AT-AgentUninstall.ps1
+++ AutomateTools/AT-AgentUninstall.ps1
@@ -3,19 +3,15 @@
 Remove-LabTech completely uninstalls the remote agent and services.
 .PARAMETER Uri
 The web address for the LT Uninversal Uninstaller zip archive.
-.PARAMETER HashUri
-The web address for a SHA256 hash of the LT Universal Uninstaller
 .EXAMPLE
-Remove-ATLabTech -Uri "https://labtech.com/LT_Uninstaller.zip" -HashUri "https://labtech.com/LT_Uninstaller_Hash.txt"
+Remove-ATLabTech -Uri "https://labtech.com/LT_Uninstaller.zip"
 .NOTES
 This function is designed so that the files can be hosted on your own server or in Amazon S3.
 #>
 Function Remove-ATLabTech {
     param(
         [Parameter (Mandatory=$True)]
-        [string] $Uri,
-        [Parameter (Mandatory=$True)]
-        [string] $HashUri
+        [string] $Uri
     )
 
     If ((Test-Path -Path "C:\AutomateTools\Downloads") -eq $False){
@@ -23,13 +19,8 @@ Function Remove-ATLabTech {
     }
 
     Invoke-WebRequest -Uri $Uri -OutFile "C:\AutomateTools\Downloads\LT_Uninstaller.zip"
-    $WebHash = $(Invoke-WebRequest -Uri $HashUri).Content
-    $FileHash = $(Get-FileHash -Path "C:\AutomateTools\Downloads\LT_Uninstaller.zip" -Algorithm "SHA256").Hash
-
-    If ($WebHash -eq $FileHash){
-        Expand-Archive -Path "C:\AutomateTools\Downloads\LT_Uninstaller.zip" -DestinationPath "C:\AutomateTools\Downloads\LT_Uninstaller" -Force
-        Start-Process -FilePath "C:\AutomateTools\Downloads\LT_Uninstaller\Agent_Uninstall.exe"
-    }
+    Expand-Archive -Path "C:\AutomateTools\Downloads\LT_Uninstaller.zip" -DestinationPath "C:\AutomateTools\Downloads\LT_Uninstaller" -Force
+    Start-Process -FilePath "C:\AutomateTools\Downloads\LT_Uninstaller\Agent_Uninstall.exe"
 }
 
 <#
